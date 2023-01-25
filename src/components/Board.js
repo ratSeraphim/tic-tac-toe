14c6bd3fedb8ae762c12b3c7c0a27fd8d40049db
ratSeraphim <telma.laudobele@accenture.com>
2023-01-25 18:37:15 +0200

    Added turns, winning functionality

--- src/components/Board.js
+++ src/components/Board.js
@@ -5,13 +5,28 @@ import Square from './Square';
 const Board = () => {
     
     const [squareVal, setSquareVal] = useState(Array(9).fill(null)) ;
-    console.log(squareVal);
-    
+    const [xIsNext, setXIsNext] = useState(true);
+
+    const winner = calculateWinner(squareVal);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+    }
     
     function renderSquare(index) {
+        
         const handleClick = (index) => {
             console.log(index);
-            setSquareVal((prevValue)=>{return [...prevValue, prevValue[index] = "X"]})
+            const tempArray = squareVal.slice();
+            if (calculateWinner(squareVal) || squareVal[index]) {
+                return;
+              }
+                tempArray[index] = xIsNext ? "X" : "O";
+            setSquareVal( tempArray);
+            setXIsNext(!xIsNext); 
+            /*setSquareVal((prevValue)=>{return [...prevValue, prevValue[index] = "X"]})*/
         } 
         return (
             <Square handleClick={handleClick} 
@@ -19,9 +34,10 @@ const Board = () => {
             
         )
     } 
-
+    
     return ( 
         <>
+        <div className="status">{status}</div>
         <div className='board-row'>
             {renderSquare(0)}
             {renderSquare(1)}
@@ -39,6 +55,26 @@ const Board = () => {
         </div>
         </>
      );
+
+     function calculateWinner(squareVal) {
+        const lines = [
+          [0, 1, 2],
+          [3, 4, 5],
+          [6, 7, 8],
+          [0, 3, 6],
+          [1, 4, 7],
+          [2, 5, 8],
+          [0, 4, 8],
+          [2, 4, 6],
+        ];
+        for (let i = 0; i < lines.length; i++) {
+          const [a, b, c] = lines[i];
+          if (squareVal[a] && squareVal[a] === squareVal[b] && squareVal[a] === squareVal[c]) {
+            return squareVal[a];
+          }
+        }
+        return null;
+      }
 }
  
 export default Board;
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,4 +1,4 @@
-import {useState } from 'react';
+
 
 const Square = ({ handleClick, tile, index}) => {
 
